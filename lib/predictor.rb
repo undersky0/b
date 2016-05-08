fec0a96d75b74666988e3972ded282dc6342a2e3
Richard D'Lonesteen <richardlonesteen@gmail.com>
2016-05-08 20:33:15 +0100

    some notes

--- lib/predictor.rb
+++ lib/predictor.rb
@@ -6,10 +6,38 @@
 # ci = second serve win percentage of player i
 # di = first service return points win percentage of player i
 # ei = second service return points win percentage of player i
+# wrp = Avenerage winning on return percentage of player i
+# as = percentage of matches won by player on  a surface a
+# bs = percentrage of matches won by a player on a surface b
+
+
+
+#get recent matches
+#where game surface is the same
+#againts players of similar calibre
+
+
+
+#the match outcome depends on the difference between the serve-winning probabilities of the two players
+
+#how to quantify players style ?
+
+#DESCRIBE MATCH RELEVANCE
+
+
+#FEATURE SELECTION: 
+
+#DATA SELECTION: check: forward selection and backward elimitaiton  select{|m| m>20 && m<200}
+
+
+
+# #In general, a player’s performance
+# improves as they gather strength and experience in the first part of their career and later declines due
+# to the physiological effects of ageing
 
 
 class Predictor
-
+  #get relevant overviews of similar calibre players
   def initialize(p1, p2)
     @p1 = Player.find_by_id p1
     @p2 = Player.find_by_id p2
